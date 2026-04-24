f4b9a7a678e78997ae3bd114690f44168c6e43f9
Tarik Hasan <tarikhasan12200@gmail.com>
2026-04-24 00:21:54 -0700

    functionCalling without Langchain

--- agent_loop_tool_calling.py
+++ agent_loop_tool_calling.py
@@ -1,10 +1,14 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from langchain.chat_models import init_chat_model
-from langchain.tools import tool
-from langchain_core.messages import SystemMessage, HumanMessage, ToolMessage
-from langchain_core.prompts import ChatPromptTemplate    
+# from langchain.chat_models import init_chat_model
+# from langchain.tools import tool
+# from langchain_core.messages import SystemMessage, HumanMessage, ToolMessage
+# from langchain_core.prompts import ChatPromptTemplate    
+# from langsmith import traceable
+
+
+import ollama
 from langsmith import traceable
 
 MAX_ITERATIONS = 10
@@ -12,7 +16,7 @@ MODEL ="qwen3:1.7b"
 
 #too/s (langchain tool decorator) to call the tool in the agent loop
 
-@tool
+@traceable(name="Get Product Price Tool", run_type="tool")
 def get_product_price(product:str)->float:
     """look up the price of a product in the catalog and return the price"""
     # Placeholder implementation - replace with actual product price lookup logic
@@ -21,11 +25,13 @@ def get_product_price(product:str)->float:
     return prices.get(product, 0.0)
 
 
-@tool
+
+
+@traceable(name="Apply Discount Tool", run_type="tool")
 def apply_discount(price:float, discount_tier:str)->float:
     """apply a discount tier to a price and return the discounted price"""
-    """Available discount tiers: Silver, Gold, Platinum"""
-    discount_percentage = {"Silver": 10, "Gold": 20, "Platinum": 30}
+    """Available discount tiers: Silver, gold, Platinum"""
+    discount_percentage = {"Silver": 10, "gold": 20, "Platinum": 30}
     discount = discount_percentage.get(discount_tier, 0)
     discounted_price = round(price * (1 - discount / 100), 2)
     print(f"Applying {discount_percentage[discount_tier]}% discount to price: {price}. Discounted price: {discounted_price}")
@@ -33,40 +39,111 @@ def apply_discount(price:float, discount_tier:str)->float:
     return discounted_price
 
 
-@traceable(name ="Langchain Agent Loop-> Tool Calling")
+tools_for_llm = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_product_price",
+            "description": "Look up the price of a product in the catalog.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "product": {
+                        "type": "string",
+                        "description": "The product name, e.g. 'laptop', 'headphones', 'keyboard'",
+                    },
+                },
+                "required": ["product"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "apply_discount",
+            "description": "Apply a discount tier to a price and return the final price. Available tiers: bronze, silver, gold.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "price": {"type": "number", "description": "The original price"},
+                    "discount_tier": {
+                        "type": "string",
+                        "description": "The discount tier: 'bronze', 'silver', or 'gold'",
+                    },
+                },
+                "required": ["price", "discount_tier"],
+            },
+        },
+    },
+]
+
+@traceable(name= "Ollama Chat", run_type="llm")
+def ollama_chat_traced(messages):
+    response = ollama.chat(MODEL, messages=messages, tools=tools_for_llm)
+    return response
+
+
+@traceable(name ="Ollama Agent Loop-> Tool Calling")
 # Agent Loop
 def run_agent(question:str):
-    tools =  [get_product_price, apply_discount]
-    tools_dict ={t.name : t for t in tools}
+    tools_dict = {
+        "get_product_price": get_product_price,
+        "apply_discount": apply_discount,
+    }
 
-    llm = init_chat_model(f"ollama:{MODEL}", temperature=0)
-    llm_with_tools = llm.bind_tools(tools)
+    # llm = init_chat_model(f"ollama:{MODEL}", temperature=0)
+    # llm_with_tools = llm.bind_tools(tools)
 
     print(f"Question: {question}")
     print("=" * 60)
 
-    message =[
-        SystemMessage(content =(
-           "You are a helpful shopping assistant. "
-            "You have access to a product catalog tool "
-            "and a discount tool.\n\n"
-            "STRICT RULES — you must follow these exactly:\n"
-            "1. NEVER guess or assume any product price. "
-            "You MUST call get_product_price first to get the real price.\n"
-            "2. Only call apply_discount AFTER you have received "
-            "a price from get_product_price. Pass the exact price "
-            "returned by get_product_price — do NOT pass a made-up number.\n"
-            "3. Never calcluate the duscounted price yourself. You MUST call apply_discount tool to get the discounted price.\n"
-            "4. if the user doesnt specify a discount tier, ask them which tier to use - do not make assumptions.\n"
-        )),
-        HumanMessage(content=question),
+    # message =[
+    #     SystemMessage(content =(
+    #        "You are a helpful shopping assistant. "
+    #         "You have access to a product catalog tool "
+    #         "and a discount tool.\n\n"
+    #         "STRICT RULES — you must follow these exactly:\n"
+    #         "1. NEVER guess or assume any product price. "
+    #         "You MUST call get_product_price first to get the real price.\n"
+    #         "2. Only call apply_discount AFTER you have received "
+    #         "a price from get_product_price. Pass the exact price "
+    #         "returned by get_product_price — do NOT pass a made-up number.\n"
+    #         "3. Never calcluate the duscounted price yourself. You MUST call apply_discount tool to get the discounted price.\n"
+    #         "4. if the user doesnt specify a discount tier, ask them which tier to use - do not make assumptions.\n"
+    #     )),
+    #     HumanMessage(content=question),
 
+    # ]
+
+    messages  = [
+        {
+            "role": "system",
+            "content": (
+                "You are a helpful shopping assistant. "
+                "You have access to a product catalog tool "
+                "and a discount tool.\n\n"
+                "STRICT RULES — you must follow these exactly:\n"
+                "1. NEVER guess or assume any product price. "
+                "You MUST call get_product_price first to get the real price.\n"
+                "2. Only call apply_discount AFTER you have received "
+                "a price from get_product_price. Pass the exact price "
+                "returned by get_product_price — do NOT pass a made-up number.\n"
+                "3. Never calcluate the duscounted price yourself. You MUST call apply_discount tool to get the discounted price.\n"
+                "4. if the user doesnt specify a discount tier, ask them which tier to use - do not make assumptions.\n"
+            )
+        },
+        {
+            "role": "user",
+            "content": question
+        },
     ]
 
     for iteration in range(MAX_ITERATIONS):
         print(f"\n--- Iteration {iteration + 1} ---")
 
-        ai_message = llm_with_tools.invoke(message)
+        response = ollama_chat_traced(messages)
+
+        ai_message = response.message
 
         tool_calls = ai_message.tool_calls
 
@@ -76,19 +153,26 @@ def run_agent(question:str):
             return ai_message.content
         
         tool_call = tool_calls[0]  # Get the first tool call
-        tool_name = tool_call.get("name")
-        tool_args = tool_call.get("args", {})
-        tool_call_id = tool_call.get("id")  
-        print(f"   [Tool Selected]: {tool_name} with args: {tool_args}")
+        tool_name = tool_call.function.name  # Get the name of the tool to call
+        tool_args = tool_call.function.arguments
+        # tool_to_use = tools_dict.get(tool_name)
+        print(f"  [Tool Selected] {tool_name} with args: {tool_args}")
 
         tool_to_use = tools_dict.get(tool_name)
         if tool_to_use is None:
             raise ValueError(f"Tool not found: {tool_name}")
-        observation = tool_to_use.invoke(tool_args)
+        
+
+        observation = tool_to_use(**tool_args)  # Call the tool with the provided arguments
         print(f"[Tool Result]': {observation}")
 
-        message.append(ai_message)
-        message.append(ToolMessage(content=observation, tool_call_id=tool_call_id)) 
+        messages.append(ai_message)
+        messages.append(
+            {
+                "role": "tool",
+                "content": str(observation),
+            }
+        )
     print("ERROR: Max iterations reached without generating a final answer.")
     return None
 
@@ -98,4 +182,4 @@ def run_agent(question:str):
 if __name__ == "__main__":
     print("Hello Langchain Agent (.bind_tools!)")
     print()
-    result = run_agent("what is the price of a laptop and apply a Gold discount?")
+    result = run_agent("what is the price of a laptop and apply a gold discount?")
